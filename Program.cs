44d6b86f3301e96fdd34ac063108d5b90c08362b
budoor <budooralsaidi1998@gmail.com>
2024-10-01 09:25:35 +0400

    add pulic class with inherent class

--- Program.cs
+++ Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-           
+
         }
     }
+
+
+    public class shape
+    {
+        protected double lengh;
+        protected double width;
+        public virtual double area(double x, double y)
+
+        {
+            return 0;
+        }
+    }
+
+    public class  square : shape 
+    {
+    
+    }
+    public class circle : shape { }
+
+    public class triangle  : shape { }
+    public class rectangle : shape { }
+}
 }
